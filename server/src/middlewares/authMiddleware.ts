b1980c56695a327c8d3d9277db4c984beb2ee344
Rami Akhmad <rami.akhmad@gmail.com>
2025-03-19 19:37:06 +0100

    refactor(authMiddleware): enhance token verification and error handling

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,28 +1,33 @@
-import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
+import jwt, { JwtPayload, VerifyErrors } from 'jsonwebtoken';
 import { NextFunction, Response, Request } from "express";
 import { TokenPayload } from "../types/express";
 
+// Middleware для проверки наличия и действительности токена
 const ifAuthenticated = (req: Request, res: Response, next: NextFunction): void => {
-    const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
-
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized: Token missing" });
+    const token = req.cookies?.token; // Извлекаем токен из cookies
+    if (token && process.env.JWT_KEY) {
+        jwt.verify(token, process.env.JWT_KEY, (err: VerifyErrors | null, decoded: JwtPayload | string | undefined) => {
+            if (err) {
+                res.status(403).json({ message: 'Forbidden: Invalid or expired token' });
+                return;
+            }
+            
+            // Если декодированный токен существует, присваиваем его в request
+            if (decoded && typeof decoded !== 'string') {
+                req.user = decoded as TokenPayload;
+                next(); // Продолжаем выполнение запроса
+            } else {
+                res.status(400).json({ message: 'Token is malformed or invalid' });
+            }
+        });
+    } else {
+        // Если токен отсутствует или некорректен
+        if (req.url !== '/check-access-token') {
+            res.status(401).json({ message: 'Unauthorized: Token missing or invalid' });
+        } else {
+            next(); // Разрешаем доступ к маршруту для проверки токена
+        }
     }
-
-    jwt.verify(token, process.env.JWT_KEY as string, (err: VerifyErrors | null, decoded: JwtPayload | string | undefined) => {
-        if (err) {
-            return res.status(err.name === "TokenExpiredError" ? 401 : 403).json({ 
-                message: err.name === "TokenExpiredError" ? "Unauthorized: Token expired" : "Forbidden: Invalid token"
-            });
-        }
-
-        if (!decoded || typeof decoded === "string") {
-            return res.status(400).json({ message: "Token is malformed" });
-        }
-
-        req.user = decoded as TokenPayload;
-        next();
-    });
 };
 
 export default ifAuthenticated;
